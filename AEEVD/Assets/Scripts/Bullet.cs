b1cc8909cf98771ca551769eb44e96bf14e29eeb
vinnycentnguyen <vinnycentnguyen@gmail.com>
2022-12-15 16:52:37 -0800

    Holding down shoot update

---
+++ AEEVD/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+        Invoke("despawn", 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+    void despawn()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ AEEVD/Assets/Weapon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float firerate;
+    float nextFire;
+    public bool holdingDown;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            holdingDown = true;
+            Shoot();
+        }
+        else{
+            holdingDown = false;
+        }
+    }
+
+    void Shoot()
+    {
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + firerate;
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        }
+    }
+}
